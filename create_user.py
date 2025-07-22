2d3f6e2306e3bbbe7eb9bfe52619d63299f8cd40
Илюшечкин Николай Витальевич <N.Ilushechkin@Teploset.int>
2025-07-22 16:05:36 +0300

    prosto tak

--- create_user.py
+++
@@ -1,9 +0,0 @@
-from app import db, User, app  # Импортируем приложение и базу данных
-
-# Создание нового пользователя
-with app.app_context():  # Оборачиваем код в контекст приложения
-    user = User(username="admin", password="admin123")
-    db.session.add(user)
-    db.session.commit()
-
-    print("Пользователь admin успешно создан!")
